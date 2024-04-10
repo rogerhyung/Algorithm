34313772d989e4d81c73ab38ca4359bd4f49c416
rogerhyung <165147498+rogerhyung@users.noreply.github.com>
2024-04-10 17:56:18 +0900

    Update 팰린드롬수.cc

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -7,8 +7,8 @@ int main() {
         int flag = 0;
         cin >> s;
         if (s == "0") break;
-        for (int i = 0; i < s.size() / 2; i++) {
-            if (s[i] != s[s.size() - 1 - i]) {
+        for (int i = 0; i < s.size() / 2; i++) { //문자열의 반까지 검사
+            if (s[i] != s[s.size() - 1 - i]) { //문자열의 중심을 기준으로 바깥쪽부터 서로 대조
                 cout << "no\n";
                 flag = 1;
                 break;
